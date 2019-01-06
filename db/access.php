f83b8097cd042bc49f020f2867ca33c25cf8ae75
Lariliel <haoogame@gmail.com>
2019-01-06 20:05:44 +0300

    2019010601
    initial commit

---
+++ db/access.php
@@ -0,0 +1,63 @@
+<?php 
+/**
+ * changelog: 2019010601 initial release
+ * @author ddsh
+ * @see https://docs.moodle.org/dev/NEWMODULE_Adding_capabilities
+ * @see https://github.com/1586-ItSolSchool/city/wiki
+ */
+$capabilities = array(
+ 
+ 'mod/city:viewalltransactions' => array(
+     'riskbitmask' => RISK_PERSONAL,
+     'captype' => 'view',
+     'contextlevel' => CONTEXT_COURSE,
+     'archetypes' => array(
+         'manager' => CAP_ALLOW
+     ),
+ ),
+ 'mod/city:editalltransactions' => array(
+    'riskbitmask' => RISK_PERSONAL,
+    'captype' => 'write',
+    'contextlevel' => CONTEXT_COURSE,
+    'archetypes' => array(
+        'manager' => CAP_ALLOW
+    ),
+),
+'mod/city:viewallcontracts' => array(
+    'riskbitmask' => RISK_PERSONAL,
+    'captype' => 'view',
+    'contextlevel' => CONTEXT_COURSE,
+    'archetypes' => array(
+        'manager' => CAP_ALLOW
+    ),
+),
+'mod/city:editallcontracts' => array(
+    'riskbitmask' => RISK_PERSONAL,
+    'captype' => 'write',
+    'contextlevel' => CONTEXT_COURSE,
+    'archetypes' => array(
+        'manager' => CAP_ALLOW
+    ),
+),
+'mod/city:commentasminecon' => array(
+    'riskbitmask' => RISK_PERSONAL,
+    'captype' => 'write',
+    'contextlevel' => CONTEXT_COURSE,
+    'archetypes' => array(
+        'manager' => CAP_ALLOW
+    ),
+),
+
+// see https://docs.moodle.org/dev/Activity_modules#access.php about this capability
+'mod/city:addinstance' => array(
+    'riskbitmask' => RISK_XSS,
+    'captype' => 'write',
+    'contextlevel' => CONTEXT_COURSE,
+    'archetypes' => array(
+        'editingteacher' => CAP_ALLOW,
+        'manager' => CAP_ALLOW
+    ),
+    'clonepermissionsfrom' => 'moodle/course:manageactivities'
+),
+);
+?>
---
+++ lang/en/city.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * changelog: 2019010601 initial release
+ * @author ddsh
+ * @see https://docs.moodle.org/dev/Activity_modules#Lang_Folder
+ */
+$string['pluginname'] = 'Digital City';
+$string['city:viewalltransactions'] = 'city:viewalltransactions';
+$string['city:editalltransactions'] = 'city:editalltransactions';
+$string['city:viewallcontracts'] = 'city:viewallcontracts';
+$string['city:editallcontracts'] = 'city:editallcontracts';
+$string['city:commentasminecon'] = 'city:commentasminecon';
+$string['city:addinstance'] = 'city:addinstance';
+?>
